1fdacda8540af6581d55a21e7f0c55a878bed42e
Wenqi He <hewenqi96@gmail.com>
2019-07-24 17:12:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-python3 -m venv venv
-. venv/bin/activate
+# python3 -m venv venv
+# . venv/bin/activate
 pip3 install -r requirements.txt
-deactivate
+# deactivate
 cd respotify && npm install
 # IMPORTANT: `build` folder is never pushed to GitHub repo
 npm run build
